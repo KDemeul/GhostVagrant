ab5c23b61a5b5617d76fd7c58027b85224c016a9
KDemeul <kilian.d@snapkin.fr>
2015-11-15 01:53:43 +0100

    New script to deploy ghost with latest release of nodjs!

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,31 +4,27 @@ then
 	touch /var/log/firsttime
 	sudo apt-get update
 
-	# Get the latest version of Node.
-	sudo apt-get -y install python-software-properties python g++ make
-	sudo add-apt-repository ppa:chris-lea/node.js
-	sudo apt-get update
-	sudo apt-get -y install nodejs
+	# Install the required package
+	apt-get install -y git
+	apt-get install -y unzip
+	curl -sL https://deb.nodesource.com/setup | sudo bash -
+	apt-get install -y nodejs
+	apt-get install -y build-essential
 
 	# Get Ghost and unzip it.
-	sudo mkdir /var/www
-	sudo mkdir /var/www/ghost
+	mkdir /var/www
+	mkdir /var/www/ghost
 	cd /var/www/ghost
-	sudo apt-get install unzip
 	wget https://en.ghost.org/zip/ghost-latest.zip
 	unzip ghost-latest.zip
+	rm ghost-latest.zip
 
-	# Create your running config and bind server socket to all IPs.
-	# cp config.example.js config.js
-	# sed -i 's/127.0.0.1/0.0.0.0/g' config.js
+	# Create config and ?????????? WHAT IS SED ???????? 
+	cp config.example.js config.js
+	sed -i 's/127.0.0.1/0.0.0.0/g' config.js
 
 	# Install Ghost.
-	sudo npm install --production
-
-	# Install forever & make ghost run forever
-	sudo npm install forever -g
-	cd /var/www/ghost
-	NODE_ENV=production forever start index.js
+	npm install --production
 fi
 
 ## SETUP SHELL
